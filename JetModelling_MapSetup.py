Fix a bug in calculation of equal merge distances across multiple maps. Update algorithm for volume calculation.

JetModelling_MapSetup.py
@@ -48,13 +48,23 @@ sDecs =      np.array([32.4255277778,
                        32.4125070566])                                      # Map centre Dec in degrees
 sSizes =     np.array([np.array([500,500]),
                        np.array([500,500]),
-                       np.array([500,1000])])                               # Source size in pixels (x,y sides of containing rectangle)
+                       np.array([500,500])])                                # Source size in pixels (x,y sides of containing rectangle)
 bgRMSs =     np.array([0.0002,
-                       0.00026,
-                       0.000313])                                           # Background flux RMS value in Jy/beam
+                       0.00036,
+                       0.00038])                                            # Background flux RMS value in Jy/beam
 bgMeans =    np.array([-0.000078874466,
-                       0.00014237006,
-                       -0.00016326427])                                     # Background flux mean pixel value in Jy/beam
+                       -0.0000017,
+                       -0.000083])                                          # Background flux mean pixel value in Jy/beam
+
+###########################################################################################################
+# Parameters used for from-data ridgelines.
+###########################################################################################################
+ridgelines_from_data_arm1s = np.array(['C:/JetModelling_FromData/3C31_1.6GHz_ridge_coords1.txt',
+                                       'C:/JetModelling_FromData/3C31_360MHz_ridge_coords1.txt',
+                                       'C:/JetModelling_FromData/3C31_615MHz_ridge_coords1.txt'])   # Input data file for arm1
+ridgelines_from_data_arm2s = np.array(['C:/JetModelling_FromData/3C31_1.6GHz_ridge_coords2.txt',
+                                       'C:/JetModelling_FromData/3C31_360MHz_ridge_coords2.txt',
+                                       'C:/JetModelling_FromData/3C31_615MHz_ridge_coords2.txt'])   # Input data file for arm2
 
 ###########################################################################################################
 # Parameters used for skeletonize ridgelines (these parameters are highly sensitive to the map).
@@ -120,7 +130,7 @@ def InitialiseMap(current_map_number):
     global map_number
     global map_file
     global rdel; global ddel; global equiv_R_factor
-    global beamsize; global beamarea
+    global beamsize; global beamarea; global max_beamsize
     global freq
     global sCentre
     global sRA; global sDec
@@ -133,6 +143,7 @@ def InitialiseMap(current_map_number):
     global MaximumLoopJumpPixels
     global nSig_s
     global SplitInnerOuterSkeleton; global nSig_s_outer; global JoinInterpolatePoints
+    global ridgelines_from_data_arm1; global ridgelines_from_data_arm2
     global ImFraction; global vmin; global vmax
 
     # Set the map number
@@ -140,6 +151,9 @@ def InitialiseMap(current_map_number):
 
     # Set the factor which determines equivalent distances along multiple jets
     equiv_R_factor = ddels[0] / ddels[map_number]
+
+    # Set the maximum beamsize for multiple jets
+    max_beamsize = np.max(beamsizes)
 
     # Initialise all map-specific parameter values for this map number
     map_file = map_files[map_number]
@@ -158,6 +172,9 @@ def InitialiseMap(current_map_number):
     MaximumLoopJumpPixels = MaximumLoopJumpPixelss[map_number]
     nSig_s = nSig_ss[map_number]
     SplitInnerOuterSkeleton = SplitInnerOuterSkeletons[map_number]; nSig_s_outer = nSig_s_outers[map_number]; JoinInterpolatePoints = JoinInterpolatePointss[map_number]
+
+    ridgelines_from_data_arm1 = ridgelines_from_data_arm1s[map_number]
+    ridgelines_from_data_arm2 = ridgelines_from_data_arm2s[map_number]
 
     ImFraction = ImFractions[map_number]; vmin = vmins[map_number]; vmax = vmaxs[map_number]
 

JetRidgeline_FromData/RidgeToolkit_FromData.py
@@ -55,11 +55,11 @@ def LoadRidgelineData(flux_array):
     """
     # Read ridgeline data file for first arm
     ridge1 = np.empty((0,2)); phi_val1 = np.empty((0)); Rlen1 = np.empty((0))
-    ridge1, phi_val1, Rlen1 = LoadRidgelineDataForOneArm(JMC.ridgelines_from_data_arm1, ridge1, phi_val1, Rlen1)
+    ridge1, phi_val1, Rlen1 = LoadRidgelineDataForOneArm(JMS.ridgelines_from_data_arm1, ridge1, phi_val1, Rlen1)
 
     # Read ridgeline data file for other arm
     ridge2 = np.empty((0,2)); phi_val2 = np.empty((0)); Rlen2 = np.empty((0))
-    ridge2, phi_val2, Rlen2 = LoadRidgelineDataForOneArm(JMC.ridgelines_from_data_arm2, ridge2, phi_val2, Rlen2)
+    ridge2, phi_val2, Rlen2 = LoadRidgelineDataForOneArm(JMS.ridgelines_from_data_arm2, ridge2, phi_val2, Rlen2)
 
     # Determine the source position and update all data to be relative to this position
     sCentre, ridge1, ridge2, Rlen1, Rlen2, phi_val1, phi_val2 = \

JetSections/JetSectionToolkit.py
@@ -600,7 +600,8 @@ def AddEdgePoints(flux_array, edge_points):
 
                 edge_points_side = np.vstack((edge_points_side, np.array([x_side1, y_side1, x_side2, y_side2, ridge_R])))
             else:
-                edge_points_side = np.array([x_side1, y_side1, x_side2, y_side2, ridge_R])
+                edge_points_side = np.empty((0,5))
+                edge_points_side = np.vstack((edge_points_side, np.array([x_side1, y_side1, x_side2, y_side2, ridge_R])))
 
             lastpts = np.array([x_side1, y_side1, x_side2, y_side2, ridge_R])
 
@@ -889,11 +890,11 @@ def MergeSections(section_parameters):
     merged_section_perimeters = np.empty((0,pSize))
 
     # Initialise minimum change in merge R (> beamsize)
-    min_delta_R = JMC.MergeMinDeltaRFactor * np.ceil(np.max(JMS.beamsize)) * JMS.equiv_R_factor
+    min_delta_R = JMC.MergeMinDeltaRFactor * np.ceil(JMS.max_beamsize)
 
-    max_R_count = 1; max_R = min_delta_R + ((JMC.MergeRIncreaseStep ** max_R_count) * JMS.equiv_R_factor)   # Initialise maximum merge distance                              
-    last_section_perimeter = []; pInsertPos = 0                                                             # Initialise last merged section perimeter list
-    last_merged_sections = np.full((1,13), np.nan)                                                          # Initialise last merged sections array
+    max_R_count = 1; max_R = ( min_delta_R + (JMC.MergeRIncreaseStep ** max_R_count) ) * JMS.equiv_R_factor     # Initialise maximum merge distance                              
+    last_section_perimeter = []; pInsertPos = 0                                                                 # Initialise last merged section perimeter list
+    last_merged_sections = np.full((1,13), np.nan)                                                              # Initialise last merged sections array
     # Initialise last merged flux/volume/area values
     last_merged_flux_section = 0.0; last_merged_volume_section = 0.0; last_merged_area_section = 0.0
 
@@ -920,7 +921,7 @@ def MergeSections(section_parameters):
 
             # Increase the maximum R until it is greater than R_section_start
             while max_R <= R_section_start:
-                max_R_count +=1; max_R += min_delta_R + ((JMC.MergeRIncreaseStep ** max_R_count) * JMS.equiv_R_factor)
+                max_R_count +=1; max_R += ( min_delta_R + (JMC.MergeRIncreaseStep ** max_R_count) ) * JMS.equiv_R_factor
 
         # Test whether the maximum merge distance R has been achieved
         if R_section_end > max_R:
@@ -963,7 +964,7 @@ def MergeSections(section_parameters):
             last_merged_flux_section = 0.0; last_merged_volume_section = 0.0; last_merged_area_section = 0.0
 
             # Set to the new maximum R
-            max_R_count +=1; max_R += min_delta_R + ((JMC.MergeRIncreaseStep ** max_R_count) * JMS.equiv_R_factor)
+            max_R_count +=1; max_R += ( min_delta_R + (JMC.MergeRIncreaseStep ** max_R_count) ) * JMS.equiv_R_factor
 
         else:
             # Adding in this section does not exceed the maximum R
@@ -1628,7 +1629,7 @@ def GetVolume(polypoints):
             # In this case, assume the volume is given by a sliced cylinder.
             phi_rdns = atan(1.0/cone_cotPhi)
             theta_rdns = atan(cone_tanTheta)
-            if (theta_rdns + JMC.geo_angtol) < (pi/2 - phi_rdns):
+            if abs(theta_rdns + JMC.geo_angtol) < (pi/2 - abs(phi_rdns)):
 
                 # Volume of top of the cone, from the vertex down to the slant plane
                 cone_volume_top = pi/3 * cone_R**2 * cone_H * \
@@ -1675,7 +1676,7 @@ def GetVolume(polypoints):
             # In this case, assume the volume is given by a sliced cylinder.
             phi_rdns = atan(1.0/cone_cotPhi)
             theta_rdns = atan(cone_tanTheta)
-            if (theta_rdns + JMC.geo_angtol) < (pi/2 - phi_rdns):
+            if abs(theta_rdns + JMC.geo_angtol) < (pi/2 - abs(phi_rdns)):
 
                 # Volume of top of the cone, from the vertex down to the slant plane
                 cone_volume_top = (pi/3 * pow(cone_h,3) * cone_cotPhi) / pow( (cone_cotPhi**2 - cone_tanTheta**2), 3/2)
@@ -1875,12 +1876,8 @@ def Setup4PointPolygon(polypoints):
                 grad_side1 = - grad_side2
             else:
                 grad_side2 = - grad_side1                                                       # Side 2 much smaller than side 1. Set to have the side 1 gradient.
-            topx_side1 = base_points[0,0] + (top_points[0,1] / grad_side1)                      # Adjust the top points
-            topx_side2 = base_points[1,0] + (top_points[1,1] / grad_side2)
-            if topx_side1 < topx_side2:
-                top_points[0,0] = topx_side1; top_points[1,0] = topx_side2
-            else:
-                top_points[0,0] = topx_side2; top_points[1,0] = topx_side1                      # Top points have crossed. Un-cross them.
+            top_points[0,0] = base_points[0,0] + (top_points[0,1] / grad_side1)                 # Adjust the top point x values
+            top_points[1,0] = base_points[1,0] + (top_points[1,1] / grad_side2)
 
     return base_points, top_points
 